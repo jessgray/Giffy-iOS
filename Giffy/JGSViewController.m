7ac8e3de9adfafbb6ce082aaaa018bb8d317a40b
Jessie Smith <jessicagraysmith@gmail.com>
2013-11-19 16:21:26 -0500

    Added files necessary for pull down menu

--- Giffy/JGSViewController.m
+++ Giffy/JGSViewController.m
@@ -9,6 +9,11 @@
 #import "JGSViewController.h"
 
 @interface JGSViewController ()
+- (IBAction)showHomeMenu:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *homeMenuButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *addGifButton;
+@property (weak, nonatomic) IBOutlet UITableView *menuView;
+
 
 @end
 
@@ -17,6 +22,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //self.menuView.hidden = YES;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +33,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showHomeMenu:(id)sender {
+    /*UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:@"Library", @"Trending Gifs", @"Logout", nil];
+    [actionSheet showFromBarButtonItem:self.addGifButton animated:YES];*/
+    
+    //self.menuView.hidden = NO;
+}
 @end
---
+++ Giffy/MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  Giffy
+//
+//  Created by Jessica Smith on 11/19/13.
+//  Copyright (c) 2013 Jessica Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UITableViewController
+
+@end
