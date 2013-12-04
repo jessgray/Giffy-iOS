aecf4d0afec1ee33f96cff135d9fa9a70a231d7a
Jessie Smith <jessicagraysmith@gmail.com>
2013-12-04 14:54:06 -0500

    Introduced a tab bar for navigation

---
+++ Giffy/LibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  LibraryViewController.h
+//  Giffy
+//
+//  Created by Jessica Smith on 12/4/13.
+//  Copyright (c) 2013 Jessica Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LibraryViewController : UIViewController
+
+@end
---
+++ Giffy/TrendingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrendingViewController.h
+//  Giffy
+//
+//  Created by Jessica Smith on 12/4/13.
+//  Copyright (c) 2013 Jessica Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrendingViewController : UIViewController
+
+@end
